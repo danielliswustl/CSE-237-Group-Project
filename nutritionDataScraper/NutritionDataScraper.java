1fe3b67523481a22f0bbaf170f93634aad88fae8
daniellis-wustl <daniel@daniels-mbp-2.attlocal.net>
2022-03-28 10:26:57 -0500

    Finished nutrition scraper for single food

--- nutritionDataScraper/NutritionDataScraper.java
+++ nutritionDataScraper/NutritionDataScraper.java
@@ -1,9 +1,16 @@
 package nutritionDataScraper;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class NutritionDataScraper {
+	private WebDriver driver;
+	
+	public void setUp() {
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//yeet
 
 	}
 
